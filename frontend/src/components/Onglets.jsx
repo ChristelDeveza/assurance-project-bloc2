58451b41db7fc77dc3ee4b8853e7f2b75a0a4a37
ChristelDeveza <chris.dvz66@gmail.com>
2023-03-11 18:26:27 +0100

    CSS desktop and mobile + correction on classname of tab

--- frontend/src/components/Onglets.jsx
+++ frontend/src/components/Onglets.jsx
@@ -14,34 +14,34 @@ function Onglets() {
     <div className="tabs-container">
       <div className="tabs">
         <div
-          className={`tab ${activeTab === 0 ? "active" : ""}`}
+          className={`tab ${activeTab === 0 && "active"}`}
           onClick={() => handleTabClick(0)}
         >
           <p>Mes informations personnelles</p>
         </div>
         <div
-          className={`tab ${activeTab === 1 ? "active" : ""}`}
+          className={`tab ${activeTab === 1 && "active"}`}
           onClick={() => handleTabClick(1)}
         >
           <p>Déclarer un sinistre</p>
         </div>
         <div
-          className={`tab ${activeTab === 2 ? "active" : ""}`}
+          className={`tab ${activeTab === 2 && "active"}`}
           onClick={() => handleTabClick(2)}
         >
           <p>Mes contrats</p>
         </div>
       </div>
       <div className="tab-content">
-        <div className={`tab-panel ${activeTab === 0 ? "active" : ""}`}>
+        <div className={`tab-panel ${activeTab === 0 && "active"}`}>
           <p>Content of Tab 1</p>
         </div>
-        <div className={`tab-panel ${activeTab === 1 ? "active" : ""}`}>
+        <div className={`tab-panel ${activeTab === 1 && "active"}`}>
           <p>
-            Content of Tab 2<Form />
+            <Form />
           </p>
         </div>
-        <div className={`tab-panel ${activeTab === 2 ? "active" : ""}`}>
+        <div className={`tab-panel ${activeTab === 2 && "active"}`}>
           <p>Content of Tab 3</p>
         </div>
       </div>
