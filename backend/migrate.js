0a85fd41635a3a8f17e172c3de84565bb5c19f15
ChristelDeveza <chris.dvz66@gmail.com>
2023-03-19 08:54:53 +0100

    connect databse 3

--- backend/migrate.js
+++ backend/migrate.js
@@ -4,14 +4,15 @@ const fs = require("fs");
 const mysql = require("mysql2/promise");
 
 const migrate = async () => {
-  const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+  const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } = process.env;
 
   const connection = await mysql.createConnection({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
+    database: DB_NAME,
+    port: DB_PORT,
     multipleStatements: true,
-    port: 3306,
   });
 
   await connection.query(`drop database if exists ${DB_NAME}`);
