7a271d7e8b55c072f05d663630809b4695e5eb27
ChristelDeveza <chris.dvz66@gmail.com>
2023-03-22 07:07:15 +0100

    pb connect

--- backend/src/controllers/ItemController.js
+++ backend/src/controllers/ItemController.js
@@ -61,6 +61,7 @@ class ItemController {
               .send({
                 id,
                 email,
+                token,
               });
           } else {
             res.status(401).send({
